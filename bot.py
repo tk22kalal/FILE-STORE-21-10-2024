1b98d58e03daff88c58eacff038189270a0be52b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-10-28 14:41:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from aiohttp import web
 from Adarsh.server import web_server
 from Adarsh.utils.keepalive import ping_server
 from Adarsh.bot.clients import initialize_clients
-from config import CHANNEL_ID
+from config import CHANNEL_ID, FORCE_SUB_CHANNEL
 import pytz
 from datetime import date, datetime
 
@@ -34,31 +34,44 @@ loop = asyncio.get_event_loop()
 
 async def start_services():
     print('\n')
-    print('------------------- Initalizing Telegram Bot -------------------')
+    print('------------------- Initializing Telegram Bot -------------------')
     bot_info = await StreamBot.get_me()
     StreamBot.username = bot_info.username
     print("------------------------------ DONE ------------------------------")
     print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
+    print("--------------- Initializing Clients ----------------")
     await initialize_clients()
     print("------------------------------ DONE ------------------------------")
     print('\n')
     print('--------------------------- Importing ---------------------------')
+    
+    # FORCE_SUB_CHANNEL check and setup
+    if FORCE_SUB_CHANNEL:
+        try:
+            link = (await StreamBot.get_chat(FORCE_SUB_CHANNEL)).invite_link
+            if not link:
+                await StreamBot.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                link = (await StreamBot.get_chat(FORCE_SUB_CHANNEL)).invite_link
+            StreamBot.invitelink = link
+        except Exception as a:
+            logging.warning(a)
+            logging.warning("Bot can't Export Invite link from Force Sub Channel!")
+            logging.warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+            logging.info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            sys.exit()
+
+    # Database channel check and setup
     try:
         db_channel = await StreamBot.get_chat(CHANNEL_ID)
         StreamBot.db_channel = db_channel
-        test = await StreamBot.send_message(chat_id=db_channel.id, text="TEST")
+        test = await StreamBot.send_message(chat_id=db_channel.id, text="Test Message")
         await test.delete()
-        print("Bot is admin in db channel")
-
     except Exception as e:
-        # Handle the exception, log it, and optionally take other actions
-        print(e)  # Print the error for debugging
-        print(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-        print("\nBot Stopped bYE")
+        logging.warning(e)
+        logging.warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+        logging.info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
         sys.exit()
+
     for name in files:
         with open(name) as a:
             patt = Path(a.name)
@@ -70,11 +83,13 @@ async def start_services():
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
             print("Tech VJ Imported => " + plugin_name)
+
     if Var.ON_HEROKU:
         print("------------------ Starting Keep Alive Service ------------------")
         print()
         asyncio.create_task(ping_server())
-    print('-------------------- Initalizing Web Server -------------------------')
+
+    print('-------------------- Initializing Web Server -------------------------')
     me = await StreamBot.get_me()
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
@@ -88,7 +103,7 @@ async def start_services():
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/NobiDeveloper')
+    print('Follow me for more such exciting bots! https://github.com/NobiDeveloper')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
@@ -96,7 +111,7 @@ async def start_services():
     print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
     print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
     if Var.ON_HEROKU:
-        print('                        app runnng on =>> {}'.format(Var.FQDN))
+        print('                        app running on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
     print('Give a star to my repo https://github.com/NobiDeveloper/Nobita-Stream-Bot  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
