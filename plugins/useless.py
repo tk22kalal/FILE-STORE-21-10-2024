2cff40c5944d2439b85775f77d27a3850e7c510b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-10-31 22:13:23 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -172,7 +172,6 @@ async def pdf_question_handler(client: Client, message: Message):
         await client.send_message(
             chat_id=message.chat.id,
             text=formatted_response,
-            parse_mode=ParseMode.HTML,
             reply_markup=inline_button
         )
     else:
