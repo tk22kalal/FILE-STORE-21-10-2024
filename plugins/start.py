b2cfdfad539f45bb28fc40578f63bec7db25c6e1
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-12-11 23:39:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -81,6 +81,7 @@ async def start_command(client: Client, message: Message):
 
         for msg in messages:
         # Clean the caption to remove any text with @ and #
+        
         original_caption = "" if not msg.caption else msg.caption.html
         cleaned_caption = re.sub(r'(\s|^)@[\w_]+', '', original_caption)
         cleaned_caption = re.sub(r'(\s|^)#[\w_]+', '', cleaned_caption).strip()
